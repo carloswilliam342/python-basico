7fa2666322e11cc0a4d88829d09af2bb877f9035
Carlos William <carloswilliamwilliam7almeida@gmail.com>
2025-03-06 13:41:11 -0300

    exercicio operadores

---
+++ operadores/exercicio-operadores.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um número:"))
+num2 = int(input("Digite mais um número:"))
+
+soma = num + num2
+produto = num * num2
+comparacao = num > num2
+
+print("A soma dos números é:", soma, "O produto dos números é:", produto, "A comparação dos números é:", comparacao)
